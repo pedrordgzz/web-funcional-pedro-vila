cc619851af37ced00f2ef69eb3c8ede762c0f8d1
Peedroorgzz <pedrojuanrodriguezz2005@gmail.com>
2025-11-21 10:56:48 +0100

    estructura base y paquetes necesarios

---
+++ web-pedro-vila/src/componentes/galeria.tsx
@@ -0,0 +1,54 @@
+import { Carousel } from 'react-bootstrap';
+
+export const GaleriaCarrusel = () => {
+  const andaluciaImages = [
+    {
+      id: 1,
+      src: '/granada.jpg',
+      alt: 'Alhambra, Granada',
+      captionTitle: 'Granada',
+      captionText: 'La majestuosa Alhambra bajo el sol.',
+    },
+    {
+      id: 2,
+      src: '/sevilla.jpg',
+      alt: 'Plaza de España, Sevilla',
+      captionTitle: 'Sevilla',
+      captionText: 'La icónica Plaza de España.',
+    },
+    {
+      id: 3,
+      src: '/cordoba.jpg',
+      alt: 'Mezquita de Córdoba',
+      captionTitle: 'Córdoba',
+      captionText: 'El histórico bosque de columnas de la Mezquita.',
+    },
+    {
+      id: 4,
+      src: '/cadiz.jpg',
+      alt: 'Playa de La Caleta, Cádiz',
+      captionTitle: 'Cádiz',
+      captionText: 'Atardecer en la playa de La Caleta.',
+    },
+    {
+      id: 5,
+      src: '/almeria.jpg',
+      alt: 'Desierto de Tabernas, Almería',
+      captionTitle: 'Almería',
+      captionText: 'Paisaje único del Desierto de Tabernas.',
+    },
+    {
+      id: 6,
+      src: '/malaga.jpg',
+      alt: 'Vistas de Málaga',
+      captionTitle: 'Málaga',
+      captionText: 'Vistas desde el Castillo de Gibralfaro.',
+    },
+    {
+      id: 7,
+      src: '/huelva.jpg',
+      alt: 'Parque de Doñana, Huelva',
+      captionTitle: 'Huelva',
+      captionText: 'Marismas en el Parque Nacional de Doñana.',
+    },
+  ];
